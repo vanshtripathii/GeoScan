910e66f3d0c08b039e9d8315e72f396e9ca62907
Vansh Tripathi <137267169+vanshtripathii@users.noreply.github.com>
2026-02-01 19:41:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -213,7 +213,7 @@ def encode_image_to_base64(image_array):
 
 if __name__ == '__main__':
 
-import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+    import os
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
